944fdb8d96cf2f2203cbb251b3ad80d05476b629
IrfanMaulanaZ <100768465+IrfanMaulanaZ@users.noreply.github.com>
2022-03-02 06:27:52 -0500

    Add files via upload

---
+++ mac_changer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import subprocess
+
+interface = input("[+] Interface EX:(wlan0) > ")
+new_mac = input("[+] Enter MAC EX:(00:11:22:33:44:55) > ")
+
+print("{+} Changging MAC address for " + interface + " to " + new_mac)
+
+subprocess.call("sudo ifconfig " + interface + " down", shell=True)
+subprocess.call("sudo ifconfig " + interface +" hw ether " + new_mac, shell=True)
+subprocess.call("sudo ifconfig " + interface + " up", shell=True)
+print("Succesfully changing MAC address...")
