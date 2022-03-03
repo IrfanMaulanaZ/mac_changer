a4d6473c59dfddecaab78c09f327382a1c415af6
IrfanMaulanaZ <100768465+IrfanMaulanaZ@users.noreply.github.com>
2022-03-02 22:39:51 -0500

    Update mac_changer.py

--- mac_changer.py
+++ mac_changer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python
-# This code is simple and easy to use because I'm new in python :)
-
 import subprocess
 
 interface = input("[+] Interface EX:(wlan0) > ")
@@ -8,7 +6,8 @@ new_mac = input("[+] Enter MAC EX:(00:11:22:33:44:55) > ")
 
 print("{+} Changging MAC address for " + interface + " to " + new_mac)
 
-subprocess.call("sudo ifconfig " + interface + " down", shell=True)
-subprocess.call("sudo ifconfig " + interface +" hw ether " + new_mac, shell=True)
-subprocess.call("sudo ifconfig " + interface + " up", shell=True)
-print("Succesfully changing MAC address...")
+subprocess.call(["sudo", "ifconfig", interface, "down"])
+subprocess.call(["sudo", "ifconfig", interface, "hw", "ether", new_mac])
+subprocess.call(["sudo", "ifconfig", interface, "up"])
+print()
+subprocess.call("ifconfig " + interface, shell=True)
